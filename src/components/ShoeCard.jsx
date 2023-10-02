684b8159a9bdd3cdfeb7afaca6ccd979ee6a30ab
MaxSM <maxi.sm117@gmail.com>
2023-10-02 16:27:48 -0300

    :zap: Add Shoe Card

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,7 +1,41 @@
-const ShoeCard = () => {
-  return (
-    <div>ShoeCard</div>
-  )
-}
+/**
+ * The ShoeCard component renders a card with an image and handles a click event to change the
+ * bigShoeImg state.
+ * @returns The `ShoeCard` component is returning a JSX element.
+ */
+const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
+  /**
+   * The handleClick function changes the bigShoeImg to imgURL.bigShoe if they are not already equal.
+   */
+  const handleClick = () => {
+    if (bigShoeImg !== imgURL.bigShoe) {
+      changeBigShoeImage(imgURL.bigShoe);
+    }
+  };
 
-export default ShoeCard
+  return (
+    <div
+      // Dinamic template string as a classname
+      /* The `className` attribute in the `div` element is using a template string to dynamically set the
+      class names based on the condition `bigShoeImg === imgURL.bigShoe`. */
+      className={`border-2 rounded-xl ${
+        bigShoeImg === imgURL.bigShoe
+          ? "border-coral-red"
+          : "border-transparent"
+      } cursor-pointer max-sm:flex-1`}
+      onClick={handleClick}
+    >
+      <div className='flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4'>
+        <img
+          src={imgURL.thumbnail}
+          alt='shoe colletion'
+          width={127}
+          height={103.34}
+          className='object-contain'
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ShoeCard;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,6 @@
-import Button from "../components/Button"
+import Button from "../components/Button";
+
+import { useState } from "react";
 
 import { arrowRight } from "../assets/icons"
 import { shoes, statistics } from "../constants"
@@ -6,6 +8,8 @@ import { bigShoe1 } from "../assets/images"
 import ShoeCard from "../components/ShoeCard"
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+
   return (
     <section
       id="home"
@@ -38,26 +42,27 @@ const Hero = () => {
       </div>
 
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
-        <img src={bigShoe1} alt="shoe collection"
+        <img src={bigShoeImg} alt="shoe collection"
          width={610}
          height={500}
          className="object-contain relative z-10"
         />
+
+
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard 
+                imgURL={shoe}
+                changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+                bigShoeImg={bigShoeImg}
+              />
+
+            </div>
+          ))}
+        </div>
+
       </div>
-
-      <div>
-        {shoes.map((shoe) => (
-          <div key={shoe}>
-            <ShoeCard 
-              imgURL={shoe}
-              changeBigShoeImage={() => {}}
-              changeBigShoeImg=""
-            />
-
-          </div>
-        ))}
-      </div>
-
     </section>
   )
 }
